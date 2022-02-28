03b5c26ef269e90e9ad2eb5bbb126de7d9ff63b6
Bearkern <haolin1224@gmail.com>
2022-02-28 16:41:32 +0800

    add productModal component, CRUD functions for cart and createOrder function

--- js/cart.js
+++ js/cart.js
@@ -1,34 +1,140 @@
-const apiUrl = 'https://vue3-course-api.hexschool.io/v2'
-const apiPath = 'clara-vue3'
+import productModal from './components/productModal.js';
+const { defineRule, Form, Field, ErrorMessage, configure } = VeeValidate;
+const { required, email, min, max } = VeeValidateRules;
+const { localize, loadLocaleFromURL } = VeeValidateI18n;
+
+defineRule('required', required);
+defineRule('email', email);
+defineRule('min', min);
+defineRule('max', max);
+
+loadLocaleFromURL('https://unpkg.com/@vee-validate/i18n@4.1.0/dist/locale/zh_TW.json');
+
+configure({
+  generateMessage: localize('zh_TW'),
+})
+
+const site = 'https://vue3-course-api.hexschool.io/v2';
+const apiPath = 'clara-vue3';
 
 const app = Vue.createApp({
-  data () {
+  data() {
     return {
-      cartData: {},
-      products: []
+      products: [],
+      productId: '',
+      cartData: {
+        carts: [],
+      },
+      loadingStatus: {
+        seeMoreInfo: '',
+        addToCart: '',
+        deleteCartItem: '',
+        updateCartItem: '',
+      },
+      form: {
+        user: {
+          name: '',
+          email: '',
+          tel: '',
+          address: '',
+        },
+        message: ''
+      }
     }
+  },
+  components: {
+    productModal,
+    vForm: Form,
+    vField: Field,
+    errorMessage: ErrorMessage,
   },
   methods: {
-    getProducts () {
-      axios.get(`${apiUrl}/api/${apiPath}/products/all`)
-        .then((res) => {
-          this.products = res.data.products
-        })
-        .catch((err) => {
-          alert(err.data.message)
-        })
+    getProducts() {
+      const loader = this.$loading.show();
+
+      axios.get(`${site}/api/${apiPath}/products/all`)
+      .then((res) => {
+        this.products = res.data.products;
+        loader.hide();
+      })
+    },
+    seeMoreInfo(id) {
+      this.loadingStatus.seeMoreInfo = id;
+      this.productId = id;
+      this.$refs.productModal.openModal();
+    },
+    getCartData() {
+      axios.get(`${site}/api/${apiPath}/cart`)
+      .then((res) => {
+        this.cartData = res.data.data;
+        this.loadingStatus.seeMoreInfo = '';
+      })
+    },
+    addToCart(id, qty = 1) {
+      const data = {
+        product_id: id,
+        qty
+      }
+
+      this.loadingStatus.addToCart = id;
+
+      axios.post(`${site}/api/${apiPath}/cart`, { data })
+      .then((res) => {
+        alert(res.data.message);
+        this.getCartData();
+        this.loadingStatus.addToCart = '';
+        this.$refs.productModal.closeModal();
+      })
+    },
+    removeCartItem(id) {
+      this.loadingStatus.removeCartItem = id;
+
+      axios.delete(`${site}/api/${apiPath}/cart/${id}`)
+      .then((res) => {
+        alert(res.data.message);
+        this.getCartData();
+        this.loadingStatus.removeCartItem = '';
+      })
+    },
+    removeCartItems() {
+      axios.delete(`${site}/api/${apiPath}/carts`)
+      .then((res) => {
+        alert(res.data.message);
+        this.getCartData();
+      })
+    },
+    updateCartItem(item) {
+      const data = {
+        product_id: item.id,
+        qty: item.qty
+      }
+
+      this.loadingStatus.updateCartItem = item.id;
+
+      axios.put(`${site}/api/${apiPath}/cart/${item.id}`, { data })
+      .then((res) => {
+        alert(res.data.message);
+        this.loadingStatus.updateCartItem = '';
+      })
+    },
+    createOrder() {
+      axios.post(`${site}/api/${apiPath}/order`, { data: this.form })
+      .then((res) => {
+        alert(res.data.message);
+        this.$refs.form.resetForm();
+        this.getCartData();
+      })
+      .catch((err) => {
+        alert(err.data.message);
+      })
     }
   },
-  mounted () {
-    this.getProducts()
+  mounted() {
+    this.getProducts();
+    this.getCartData();
   }
 })
 
-VeeValidate.defineRule('email', VeeValidateRules.email)
-VeeValidate.defineRule('required', VeeValidateRules.required)
+app.use(VueLoading.Plugin);
 
-app.component('VForm', VeeValidate.Form)
-app.component('VField', VeeValidate.Field)
-app.component('ErrorMessage', VeeValidate.ErrorMessage)
-
-app.mount('#app')
+app.mount('#app');
